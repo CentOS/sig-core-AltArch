69c03260f9af439482490f0b095f05de0f3f34bd
Pablo Greco <pablo@fliagreco.com.ar>
2019-03-24 22:12:41 +0000

    Change image name to 1810

--- image_build/build-armhfp-img.sh
+++ image_build/build-armhfp-img.sh
@@ -8,5 +8,5 @@ fi
 
 img=$(echo $ks|rev|cut -f 1 -d "/"|rev|sed s/\.ks//g)
 
-time appliance-creator --config=${ks} --name="CentOS-Userland-7-armv7hl-$img-1804" --version="7" --debug
+time appliance-creator --config=${ks} --name="CentOS-Userland-7-armv7hl-$img-1810" --version="7" --debug
 
